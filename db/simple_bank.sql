ccc4ab1cb7f922ac54141044892ab9377542d75e
Peter DADA <dapetoo@gmail.com>
2023-10-07 18:47:01 +0100

    Update schema

--- db/simple_bank.sql
+++ db/simple_bank.sql
@@ -1,11 +1,11 @@
-create TABLE "entries" (
+CREATE TABLE "entries" (
   "id" bigserial PRIMARY KEY NOT NULL,
-  "account_id" bigint,
+  "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-create TABLE "accounts" (
+CREATE TABLE "accounts" (
   "id" bigserial PRIMARY KEY NOT NULL,
   "owner" varchar NOT NULL,
   "balance" bigint NOT NULL,
@@ -13,30 +13,30 @@ create TABLE "accounts" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-create TABLE "transfers" (
+CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY NOT NULL,
-  "from_account_id" bigint,
-  "to_account_id" bigint,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-create index on "entries" ("account_id");
+CREATE INDEX ON "entries" ("account_id");
 
-create index on "accounts" ("owner");
+CREATE INDEX ON "accounts" ("owner");
 
-create index on "transfers" ("from_account_id");
+CREATE INDEX ON "transfers" ("from_account_id");
 
-create index on "transfers" ("to_account_id");
+CREATE INDEX ON "transfers" ("to_account_id");
 
-create index on "transfers" ("from_account_id", "to_account_id");
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
-comment on column "entries"."amount" is 'can be positive or negative';
+COMMENT ON COLUMN "entries"."amount" IS 'can be positive or negative';
 
-comment on column "transfers"."amount" is 'must be positive';
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
 
-alter table "entries" add FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
-alter table "transfers" add FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
-alter table "transfers" add FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
