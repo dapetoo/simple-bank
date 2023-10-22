1dd8db4c5619cf564a9ab0cf1b8c3372cbb12152
Peter DADA <dapetoo@gmail.com>
2023-10-22 12:33:56 +0100

    Unit test for CreateEntry

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dapetoo/simple-bank/util"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func createRandomEntry(t *testing.T, account Account) Entry {
@@ -24,4 +25,26 @@ func createRandomEntry(t *testing.T, account Account) Entry {
 	require.NotZero(t, entry.ID)
 	require.NotZero(t, entry.CreatedAt)
 	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry1 := createRandomEntry(t, account)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+
 }
