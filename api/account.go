091ce57a1bf25eae2e950408f8b27e8936d9bf57
Peter DADA <dapetoo@gmail.com>
2023-11-20 08:39:18 +0100

    GetAccount function implementation

--- api/account.go
+++ api/account.go
@@ -59,7 +59,7 @@ func (server *Server) getAccount(ctx *gin.Context) {
 
 type listAccountRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
-	PageSize int32 `form:"page_size" binding:"require,min=5,max=10"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) listAccount(ctx *gin.Context) {
